f3cd5ecea22887f2c44feffb3ff1915c64660ecb
Dawid Biel <dawid.biel@fujitsu.com>
2024-10-01 00:52:06 +0200

    Create first faker by Bogus

---
+++ Cartelmen.Infrastructure/Seeds/DataGenerator.cs
@@ -0,0 +1,30 @@
+﻿using Bogus;
+using Cartelmen.Domain.Entities;
+using Cartelmen.Infrastructure.Persistence;
+
+namespace Cartelmen.Infrastructure.Seeds
+{
+    public class DataGenerator
+    {
+        public void Seed(CartelmenDbContext context)
+        {
+            var locale = "pl";
+            Randomizer.Seed = new Random(777);
+
+            var addressGenerator = new Faker<Address>(locale)
+                .RuleFor(a => a.Country, f => f.Address.Country())
+                .RuleFor(a => a.City, f => f.Address.City())
+                .RuleFor(a => a.Street, f => f.Address.StreetName())
+                .RuleFor(a => a.PostalCode, f => f.Address.ZipCode());
+
+            var address = addressGenerator.Generate();
+
+            var id = 1;
+            var buildingGenerator = new Faker<Building>(locale)
+                .StrictMode(true)
+                .RuleFor(b=>b.Id, f => id++);
+
+
+        }
+    }
+}
