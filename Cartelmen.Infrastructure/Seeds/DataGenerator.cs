ae691acc31496b28f220109ca5742817d6153212
Dawid Biel <dawid.biel@fujitsu.com>
2024-10-03 16:36:05 +0200

    Update data seeding

--- Cartelmen.Infrastructure/Seeds/DataGenerator.cs
+++ Cartelmen.Infrastructure/Seeds/DataGenerator.cs
@@ -2,6 +2,8 @@
 using Bogus.DataSets;
 using Cartelmen.Domain.Entities;
 using Cartelmen.Infrastructure.Persistence;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
 using Entity = Cartelmen.Domain.Entities;
 
 namespace Cartelmen.Infrastructure.Seeds
@@ -21,7 +23,7 @@ namespace Cartelmen.Infrastructure.Seeds
             Randomizer.Seed = new Random(777);
 
             if (!await _dbContext.Database.CanConnectAsync()) return;
-            if (_dbContext.Buildings.Any()) return;
+            
 
 
             var workerGenerator = new Faker<Worker>(Locale)
@@ -54,12 +56,57 @@ namespace Cartelmen.Infrastructure.Seeds
                 .RuleFor(b => b.Description, f => f.Company.CatchPhrase())
                 .RuleFor(b => b.StartDate, f => f.Date.BetweenDateOnly(DateOnly.Parse("2024-01-01"),DateOnly.Parse("2024-12-31")).OrNull(f,.1f))
                 .RuleFor(b => b.Address, () => addressGenerator.Generate())
-                .RuleFor(b => b.Workers, f => f.PickRandom(workers,8).ToList());
+                .RuleFor(b => b.Workers, f => f.PickRandom(workers, 8).ToList());
 
-            var dataSeed = buildingGenerator.Generate(5);
 
-             await _dbContext.AddRangeAsync(dataSeed);
-             await _dbContext.SaveChangesAsync();
+            //buildings
+            List<Building> buildings;
+            if (_dbContext.Buildings.Any())
+            {
+                buildings = await _dbContext.Buildings.ToListAsync();
+            }
+            else
+            {
+                buildings = buildingGenerator.Generate(5).ToList();
+
+                await _dbContext.AddRangeAsync(buildings);
+                await _dbContext.SaveChangesAsync();
+            }
+
+
+            //timetracks
+            if (!_dbContext.TimeTracks.Any()) return;
+
+            var timeTrackGenerator = new Faker<TimeTrack>(Locale)
+                .Rules((f, tt) =>
+                {
+                    tt.WorkDate = f.Date.RecentDateOnly(7);
+                    tt.WorkHours = f.Random.Int(0, 24 * 4) * 0.25m;
+                    var building = f.PickRandom(buildings);
+                    tt.BuildingId = building.Id;
+                    tt.WorkerId = f.PickRandom(building.Workers).Id;
+                });
+
+
+            var timeTracks = Enumerable.Empty<TimeTrack>().ToList();
+            do
+            {
+                var timeTrack = timeTrackGenerator.Generate();
+
+                var keyExists = timeTracks
+                    .Any(tt => $"{tt.WorkDate}{tt.BuildingId}{tt.WorkerId}" == $"{timeTrack.WorkDate}{timeTrack.BuildingId}{timeTrack.WorkerId}");
+
+                if (!keyExists)
+                {
+                    timeTracks.Add(timeTrack);
+                }
+            } while (timeTracks.Count <= 100);
+
+
+            await _dbContext.AddRangeAsync(timeTracks);
+            await _dbContext.SaveChangesAsync();
+
+
         }
 
         
