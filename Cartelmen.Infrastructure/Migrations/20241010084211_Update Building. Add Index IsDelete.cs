a01afbff1e16fd6f47209041ac420d469e316544
Dawid Biel <dawid.biel@fujitsu.com>
2024-10-10 10:43:27 +0200

    Add migration Building.IsDelete. Create Index for [IsDelete]

---
+++ Cartelmen.Infrastructure/Migrations/20241010084211_Update Building. Add Index IsDelete.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cartelmen.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateBuildingAddIndexIsDelete : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Buildings_IsDeleted",
+                table: "Buildings",
+                column: "IsDeleted",
+                filter: "IsDeleted = 0");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Buildings_IsDeleted",
+                table: "Buildings");
+        }
+    }
+}
