e8675a9e87b93cc39536d5f32f3ddda4078a0873
Dawid Biel <dawidbiell@gmail.com>
2025-09-17 23:08:03 +0200

    Displaying the fetched users in the browser

--- cartelmen-ui/src/app/app.ts
+++ cartelmen-ui/src/app/app.ts
@@ -1,12 +1,29 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('cartelmen-ui');
+export class App implements OnInit {
+  private httpClient = inject(HttpClient);
+  protected readonly title = signal('Cartelmen App');
+  protected buildings = signal<any>([]);
+
+  async ngOnInit() {
+    this.buildings.set(await this.getBuildings());
+  }
+
+  async getBuildings() {
+    try {
+      return lastValueFrom(this.httpClient.get('http://localhost:5000/api/buildings'))
+    } catch (error) {
+      console.error('Error fetching buildings:', error);
+      throw error;
+    }
+  }
 }
