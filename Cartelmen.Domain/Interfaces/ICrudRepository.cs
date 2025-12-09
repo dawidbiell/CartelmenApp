034ec0d443e877592985a441165511245c7bc153
Dawid Biel <dawidbiell@gmail.com>
2025-12-09 23:22:42 +0100

    ICrudRepository.cs | add interface

---
+++ Cartelmen.Domain/Interfaces/ICrudRepository.cs
@@ -0,0 +1,10 @@
+namespace Cartelmen.Domain.Interfaces;
+
+public interface ICrudRepository<T>
+{
+    Task<T?> AddAsync(T? entity, CancellationToken cancellationToken = default);
+    Task<IEnumerable<T>> GetAllAsync(CancellationToken cancellationToken  = default);
+    Task<T?> GetByIdAsync(int id, CancellationToken cancellationToken  = default);
+    Task<T?> UpdateAsync(T? entity, CancellationToken cancellationToken  = default);
+    Task<bool> DeleteByIdAsync(Guid id, CancellationToken cancellationToken  = default);
+}
