2c5926ba232bf91397dbcb313626f26c7126e0c1
Dawid Biel <dawidbiell@gmail.com>
2026-03-26 22:44:01 +0100

    feat: add lists and messages components with associated HTML and CSS files

---
+++ cartelmen-ui/src/features/spots/spot-detalied/spot-detalied.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-spot-detalied',
+  imports: [],
+  templateUrl: './spot-detalied.html',
+  styleUrl: './spot-detalied.css'
+})
+export class SpotDetalied {
+
+}
---
+++ cartelmen-ui/src/features/spots/spot-list/spot-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-spot-list',
+  imports: [],
+  templateUrl: './spot-list.html',
+  styleUrl: './spot-list.css'
+})
+export class SpotList {
+
+}
